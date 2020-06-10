2955b55f6516fa0a82b62a5cbf17bd4cdfcf907a
Evelin Szabados <szabadosevelin7@gmail.com>
2020-06-10 15:25:42 +0200

    Change cookie headers

--- src/main/java/com/codecool/rent_manager/controller/AuthController.java
+++ src/main/java/com/codecool/rent_manager/controller/AuthController.java
@@ -65,6 +65,10 @@ public class AuthController {
                 .path("/")
                 .build();
         response.addHeader("Set-Cookie", cookie.toString());
+        response.addHeader("Access-Control-Allow-Origin", "http://rent-manager.netlify.app");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE");
+        response.addHeader("Access-Control-Allow-Headers"," Content-Type, *");
 
     }
 
